0204c5ca150119c3dcfa05e4d08b584aefe5b4aa
mehadisa18 <lmehadisa@gmail.com>
2018-01-24 19:36:30 -0600

    My First commit in python project

---
+++ 02_StringsInPython.py
@@ -0,0 +1,12 @@
+message1 = "Hello this is a string with double quotes"
+message2 = 'Hello this is string with single quotes'
+message3="Hello this is a string where there are 'single quotes' with in double quotes"
+message4='Hello this is a string where there are "double quotes" with in single quotes'
+message5='Here I\'m escaping the aposthrophe with a back slash'
+message6=""" here is a sentence which contains both 'single' and "double quotes" """
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+print(message5)
+print(message6)
