5725a04fae234af899ab099859228ebb8b9df14b
mehadisa18 <lmehadisa@gmail.com>
2018-02-06 14:26:13 -0600

    Numbers in python3

--- 03_NumbersInPython3.py
+++ 03_NumbersInPython3.py
@@ -7,4 +7,23 @@
 # This doesnt work in python3
 
 a = 123394954909685650579467974068098547607909670979795724675378399999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-print("type of a "+a)
+print(a)
+print(type(a)) # type() is used to find out the type of any value
+
+a="message"
+print(type(a))
+
+# In python3 we have int, float and complex numbers.
+# here for complex numbers we use j instead of i 
+# say c = 2 + 3j
+# lets create some numbers.
+
+b = 23.78 # float
+c= 23 + 3j # complex number
+
+print(type(b))
+print(type(c))
+print (c.real)
+print(c.imag)
+print("complex numbers"+str(c)) # for concatenation the values of different datatypes have to be converted into
+                                # strings before printing. use str() function for that.
